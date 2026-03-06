44dbaf13828f3cac23196311a4684786d5874078
abdullahansari-yk <workingabd62@gmail.com>
2026-03-06 15:58:00 +0530

    Completed Bg-color project where user can change as well as new buttons

--- webdev-devops/Day1/HTML-CSS-Assignment/Assignment-2/Easy/bg-color-changer/index.js
+++ webdev-devops/Day1/HTML-CSS-Assignment/Assignment-2/Easy/bg-color-changer/index.js
@@ -1,18 +1,46 @@
 const container = document.querySelector("#container");
 const buttons = document.querySelectorAll(".colorButton");
+const addBtn = document.querySelector("#add");
+const controlPanel = document.querySelector("#colorPanel");
 
-// console.log(buttons)
+controlPanel.addEventListener("click", (e) => {
+  if (e.target.classList.contains("colorButton")) {
+    container.style.backgroundColor = e.target.id;
+  }
+});
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    // console.log(e.target.id);
-
     container.style.backgroundColor = e.target.id;
   });
 });
 
-const add=document.querySelector("#add")
-// console.log(add)
-add.addEventListener('click',(e)=>{
-    console.log(e.target)
-})
+addBtn.addEventListener("click", (e) => {
+  //creatng input to get the button(color) name.
+  const inputField = document.createElement("input");
+  inputField.type = "text";
+  inputField.classList.add("inputfield");
+  controlPanel.appendChild(inputField);
+
+  // button will created once user done typing name and hit enter.
+  inputField.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      console.log(e.target.value);
+
+      const div = document.createElement("div");
+      div.classList.add("colorButton");
+      div.id = e.target.value;
+      div.textContent =
+        e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
+      div.style.backgroundColor = e.target.value;
+
+      div.addEventListener("click", (e) => {
+        container.style.backgroundColor = e.target.id;
+      });
+      controlPanel.appendChild(div);
+
+      // this will remove the inputfiled
+      controlPanel.removeChild(inputField);
+    }
+  });
+});
