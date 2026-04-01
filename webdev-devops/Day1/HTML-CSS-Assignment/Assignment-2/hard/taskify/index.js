ff8502f29efc8d218be6259ca56481bff6b22411
abdullahansari-yk <workingabd62@gmail.com>
2026-04-01 13:14:28 +0530

    Added new card functionality

--- webdev-devops/Day1/HTML-CSS-Assignment/Assignment-2/hard/taskify/index.js
+++ webdev-devops/Day1/HTML-CSS-Assignment/Assignment-2/hard/taskify/index.js
@@ -2,34 +2,178 @@ const btns = document.querySelectorAll(".AddBtn");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    console.log(e.target);
+    // if user clcik on any btn, first check from which category it is clicked
 
-    //  when user click on add btn
+    if (btn.parentElement.id == "todo") {
+      //   console.log("it is todo section");
 
-    // create label and input
-    const label = document.createElement("label");
-    label.innerText = "Todo name :";
-    const input = document.createElement("input");
-    input.type = "text";
+      const card = document.createElement("div");
+      card.classList.add("card");
 
-    const div = document.createElement("div");
-    div.id = "takingInput1";
-    div.append(label, input);
+      const cards = document.querySelector(".todoCards");
+      cards.appendChild(card);
 
-    // create label and input
-    const label2 = document.createElement("label");
-    label2.innerText = "Todo description? :";
-    const input2 = document.createElement("input");
-    input2.type = "text";
+      const inputField1 = document.createElement("input");
+      inputField1.placeholder = "Enter Name";
 
-    const div2 = document.createElement("div");
-    div2.id = "takingInput2";
-    div2.append(label2, input2);
+      const inputField2 = document.createElement("input");
+      inputField2.placeholder = "Enter description";
 
-    // append
-    const btnParent = btn.parentElement;
-    btnParent.append(div, div2);
-    console.log(btnParent);
+      card.append(inputField1, inputField2);
 
+      inputField1.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          const title = document.createElement("div");
+          title.classList.add("title");
+          title.innerHTML = e.target.value;
+
+          card.appendChild(title);
+
+          //   delete input field
+          inputField1.parentElement.removeChild(inputField1);
+          //   console.log(inputField1.parentElement.removeChild(inputField1));
+        }
+      });
+
+      inputField2.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          // console.log(e.target.value);
+          const description = document.createElement("div");
+          description.classList.add("description");
+          description.innerHTML = e.target.value;
+          console.log(e.target.value);
+
+          card.appendChild(description);
+          //   delete input field
+          inputField2.parentElement.removeChild(inputField2);
+        }
+      });
+    } else if (btn.parentElement.id == "inProgress") {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const cards = document.querySelector(".inProgressCards");
+      cards.appendChild(card);
+
+      const inputField1 = document.createElement("input");
+      inputField1.placeholder = "Enter Name";
+
+      const inputField2 = document.createElement("input");
+      inputField2.placeholder = "Enter description";
+
+      card.append(inputField1, inputField2);
+
+      inputField1.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          const title = document.createElement("div");
+          title.classList.add("title");
+          title.innerHTML = e.target.value;
+
+          card.appendChild(title);
+
+          //   delete input field
+          inputField1.parentElement.removeChild(inputField1);
+          //   console.log(inputField1.parentElement.removeChild(inputField1));
+        }
+      });
+
+      inputField2.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          // console.log(e.target.value);
+          const description = document.createElement("div");
+          description.classList.add("description");
+          description.innerHTML = e.target.value;
+          console.log(e.target.value);
+
+          card.appendChild(description);
+          //   delete input field
+          inputField2.parentElement.removeChild(inputField2);
+        }
+      });
+    } else if (btn.parentElement.id == "underReview") {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const cards = document.querySelector(".underReviewCards");
+      cards.appendChild(card);
+
+      const inputField1 = document.createElement("input");
+      inputField1.placeholder = "Enter Name";
+
+      const inputField2 = document.createElement("input");
+      inputField2.placeholder = "Enter description";
+
+      card.append(inputField1, inputField2);
+
+      inputField1.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          const title = document.createElement("div");
+          title.classList.add("title");
+          title.innerHTML = e.target.value;
+
+          card.appendChild(title);
+
+          //   delete input field
+          inputField1.parentElement.removeChild(inputField1);
+          //   console.log(inputField1.parentElement.removeChild(inputField1));
+        }
+      });
+
+      inputField2.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          // console.log(e.target.value);
+          const description = document.createElement("div");
+          description.classList.add("description");
+          description.innerHTML = e.target.value;
+          console.log(e.target.value);
+
+          card.appendChild(description);
+          //   delete input field
+          inputField2.parentElement.removeChild(inputField2);
+        }
+      });
+    } else {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const cards = document.querySelector(".finishedCards");
+      cards.appendChild(card);
+
+      const inputField1 = document.createElement("input");
+      inputField1.placeholder = "Enter Name";
+
+      const inputField2 = document.createElement("input");
+      inputField2.placeholder = "Enter description";
+
+      card.append(inputField1, inputField2);
+
+      inputField1.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          const title = document.createElement("div");
+          title.classList.add("title");
+          title.innerHTML = e.target.value;
+
+          card.appendChild(title);
+
+          //   delete input field
+          inputField1.parentElement.removeChild(inputField1);
+          //   console.log(inputField1.parentElement.removeChild(inputField1));
+        }
+      });
+
+      inputField2.addEventListener("keydown", (e) => {
+        if (e.key == "Enter") {
+          // console.log(e.target.value);
+          const description = document.createElement("div");
+          description.classList.add("description");
+          description.innerHTML = e.target.value;
+          console.log(e.target.value);
+
+          card.appendChild(description);
+          //   delete input field
+          inputField2.parentElement.removeChild(inputField2);
+        }
+      });
+    }
   });
 });
