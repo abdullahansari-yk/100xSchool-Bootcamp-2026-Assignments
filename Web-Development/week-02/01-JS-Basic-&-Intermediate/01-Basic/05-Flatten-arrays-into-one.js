b582fb9317a653fbf4d2e94c3259a463099f1326
abdullahansari-yk <workingabd62@gmail.com>
2026-04-06 23:22:44 +0530

    Added one more solution to 05-Flatten-Arrays-into-one

--- Web-Development/week-02/01-JS-Basic-&-Intermediate/01-Basic/05-Flatten-arrays-into-one.js
+++ Web-Development/week-02/01-JS-Basic-&-Intermediate/01-Basic/05-Flatten-arrays-into-one.js
@@ -8,6 +8,15 @@
 
 const input = { fruits: ["apple", "banana"], veggies: ["carrot", "pea"] };
 
-
 const arr = Object.values(input).flat();
 console.log(arr);
+
+// solution 2 : cerate new Array and psh key and values inside it.
+
+const arr2 = Object.values(input);
+const newArr = [];
+const arr3 = arr.map(([key, value]) => {
+  newArr.push(key);
+  newArr.push(value);
+});
+console.log(newArr);
