83f6e7cc4e90752c1762a31cde36b605c095e101
abdullahansari-yk <workingabd62@gmail.com>
2026-04-13 10:33:42 +0530

    Done 13-Count-Number-of-keys-in-objects.js

---
+++ Web-Development/week-02/01-JS-Basic-&-Intermediate/01-Basic/13-count-no-of-keys.js
@@ -0,0 +1,13 @@
+/* 
+    Question: Count number of keys in object
+    
+    input : { a: 1, b: 2, c: 3 }
+    output: 3
+  
+*/
+
+// solution :
+
+const input = { a: 1, b: 2, c: 3 };
+
+console.log(Object.keys(input).length);
