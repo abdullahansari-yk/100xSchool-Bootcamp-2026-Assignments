10a0bd9eefdd3ce9860a53bbeee425ca5c71c7e1
abdullahansari-yk <workingabd62@gmail.com>
2026-04-15 21:59:25 +0530

    Done 01-sum-transaction-per-user

---
+++ Web-Development/week-02/01-JS-Basic-&-Intermediate/02-Intermediate/01-sum-all-transaction.js
@@ -0,0 +1,33 @@
+/*
+    question : Sum all transactions per user
+    input : [
+        { user: "A", amount: 100 },
+        { user: "B", amount: 200 },
+        { user: "A", amount: 50 }
+    ]
+
+    output : { A: 150, B: 200 }
+*/
+
+// solution :
+
+const input = [
+  { user: "A", amount: 100 },
+  { user: "B", amount: 200 },
+  { user: "A", amount: 50 },
+];
+
+const ans = input.reduce((acc, curr) => {
+  const user = curr.user;
+
+  const amount = curr.amount;
+
+  if (acc[user]) {
+  } else {
+    acc[user] = amount;
+  }
+
+  return acc;
+}, {});
+
+console.log(ans);
